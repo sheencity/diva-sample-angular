3df119381f0d9c9d4f44931530be27f4b6769540
王子实 <wangzishi@corp.sheencity.com>
2021-04-17 11:12:33 +0800

    fix: 构建错误

--- src/app/common/services/diva.service.ts
+++ src/app/common/services/diva.service.ts
@@ -1,22 +1,24 @@
 import { Injectable } from '@angular/core';
-// import { DivaClient } from '@sheencity/diva-sdk/lib/clients/diva.client';
+import { Diva } from '@sheencity/diva-sdk';
+
+type Awaited<T> = T extends Promise<infer U> ? U : never;
 
 @Injectable({
   providedIn: 'root',
 })
 export class DivaService {
-  // client?: DivaClient;
+  client?: Awaited<ReturnType<Diva['init']>>;
 
   constructor() {}
 
   async init(container: HTMLElement) {
-    console.log({container});
-    // const uri = 'http://wuhan.corp.sheencity.com:3000';
-    // const apiKey = 'ZWYwMTM2YmUtYzkxMi00OWQzLTlkZDEtODMxY2QzY2FkZmM4';
-    // console.log(uri, apiKey, container);
-    // const diva = new Diva({ container, apiKey, uri });
-    // console.log('diva is', diva);
-    // const client = await diva.init();
-    // console.log('client is', client);
+    console.log({ container });
+    const uri = 'http://wuhan.corp.sheencity.com:3000';
+    const apiKey = 'ZWYwMTM2YmUtYzkxMi00OWQzLTlkZDEtODMxY2QzY2FkZmM4';
+    console.log(uri, apiKey, container);
+    const diva = new Diva({ container, apiKey, uri });
+    console.log('diva is', diva);
+    this.client = await diva.init();
+    console.log('client is', this.client);
   }
 }
