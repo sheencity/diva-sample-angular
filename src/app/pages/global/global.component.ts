b00f4e87e1f97e71405e0df70ccc50a20142d577
郝强强 <haoqiangqiang@sheencity.com>
2021-04-19 08:45:11 +0800

    feat(diva-sdk): 完成全局配置界面

--- src/app/pages/global/global.component.ts
+++ src/app/pages/global/global.component.ts
@@ -1,4 +1,5 @@
 import { Component, OnInit } from '@angular/core';
+import { DropdownData } from 'src/app/common/dtos/dropdown-data.interface';
 
 @Component({
   selector: 'app-global',
@@ -6,6 +7,44 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./global.component.scss']
 })
 export class GlobalComponent implements OnInit {
+  // 罗盘
+  private _compass = false;
+  public set compass(v: boolean) {
+    console.log('compass', v);
+    // 此处设置罗盘开关
+    this._compass = v;
+  }
+  public get compass() {
+    return this._compass;
+  }
+
+  // 镜头旋转
+  private _rotation = false;
+  public set rotation(v: boolean) {
+    console.log('rotation', v);
+    // 此处设置镜头旋转开关
+    this._rotation = v;
+  }
+  public get rotation() {
+    return this._rotation;
+  }
+
+  // 模式
+  private _selectedMode: DropdownData = { value: 'false', placeholder: '飞行' };
+  public set selectedMode(v: DropdownData) {
+    const active = v.value === 'true' ? true : false;
+    console.log('第三人称模式是', active)
+    // 此处设置第三人称
+    this._selectedMode = v;
+  }
+  public get selectedMode() {
+    return this._selectedMode;
+  }
+
+  options = [
+    { value: 'false', placeholder: '飞行' },
+    { value: 'true', placeholder: '第三人称' },
+  ];
 
   constructor() { }
 
