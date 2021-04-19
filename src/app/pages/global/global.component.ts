7334628bf9103ac8a5281a184b49ec6202d03749
Raofei <raofei@sheencity.com>
2021-04-19 17:22:00 +0800

    feat(diva): 全局设置接口调用

--- src/app/pages/global/global.component.ts
+++ src/app/pages/global/global.component.ts
@@ -1,6 +1,7 @@
 import { Component, OnDestroy, OnInit } from '@angular/core';
 import { DropdownData } from 'src/app/common/dtos/dropdown-data.interface';
 import { DataService } from 'src/app/common/services/data.service';
+import { DivaService } from 'src/app/common/services/diva.service';
 
 @Component({
   selector: 'app-global',
@@ -12,6 +13,7 @@ export class GlobalComponent implements OnInit, OnDestroy {
   private _compass: boolean;
   public set compass(v: boolean) {
     console.log('compass', v);
+    this._diva.client.setCompass(v);
     this._compass = v;
     this._data.compass = v;
     // 此处设置罗盘开关
@@ -24,6 +26,10 @@ export class GlobalComponent implements OnInit, OnDestroy {
   private _rotation: boolean;
   public set rotation(v: boolean) {
     console.log('rotation', v);
+    this._diva.client.request('RotateAroundTheCenter',{
+      direction: v ? 'clockwise' : 'stop',
+      duration: 40,
+    });
     this._rotation = v;
     this._data.rotation = v;
     // 此处设置镜头旋转开关
@@ -39,7 +45,10 @@ export class GlobalComponent implements OnInit, OnDestroy {
     this._data.selectedMode = v;
     const active = v.value === 'true' ? true : false;
     console.log('第三人称模式是', active)
-    // 此处设置第三人称
+    this._diva.client.request('ActiveThirdPersonMode', {
+      'active': v.value == 'true'
+    })
+    this._selectedMode = v;
   }
   public get selectedMode() {
     return this._selectedMode;
@@ -50,7 +59,7 @@ export class GlobalComponent implements OnInit, OnDestroy {
     { value: 'true', placeholder: '人视' },
   ];
 
-  constructor(private _data: DataService) { }
+  constructor(private _data: DataService, private _diva: DivaService) { }
 
   ngOnInit(): void {
     this._selectedMode = this._data.selectedMode;
