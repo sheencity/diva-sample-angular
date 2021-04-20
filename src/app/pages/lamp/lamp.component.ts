81a8050824937e93dda63e7fe7be81e6752813ea
郝强强 <haoqiangqiang@sheencity.com>
2021-04-20 15:02:22 +0800

    feat(diva-sdk): 完成灯光控制

--- src/app/pages/lamp/lamp.component.ts
+++ src/app/pages/lamp/lamp.component.ts
@@ -17,14 +17,12 @@ export class LampComponent implements OnInit, OnDestroy {
   public roadController2: DeviceController;
   public roadController3: DeviceController;
   public roadController4: DeviceController;
-  public lampState: boolean[];
   // 路灯1设置
-  private _road1 = false;
+  private _road1 = true;
   public set road1(v: boolean) {
-    console.log('road1', v);
+    if (!this.roadController1) return;
     // 此处设置路灯1开关
     v ? this.roadController1.turnOn() : this.roadController1.turnOff();
-    this.lampState[0] = v;
     this._road1 = v;
   }
   public get road1() {
@@ -32,12 +30,11 @@ export class LampComponent implements OnInit, OnDestroy {
   }
 
   // 路灯2设置
-  private _road2 = false;
+  private _road2 = true;
   public set road2(v: boolean) {
-    console.log('road2', v);
+    if (!this.roadController2) return;
     // 此处设置路灯2
     v ? this.roadController2.turnOn() : this.roadController2.turnOff();
-    this.lampState[1] = v;
     this._road2 = v;
   }
   public get road2() {
@@ -45,12 +42,11 @@ export class LampComponent implements OnInit, OnDestroy {
   }
 
   // 路灯3设置
-  private _road3 = false;
+  private _road3 = true;
   public set road3(v: boolean) {
-    console.log('road', v);
+    if (!this.roadController3) return;
     // 此处设置路灯3开关
     v ? this.roadController3.turnOn() : this.roadController3.turnOff();
-    this.lampState[2] = v;
     this._road3 = v;
   }
   public get road3() {
@@ -58,12 +54,11 @@ export class LampComponent implements OnInit, OnDestroy {
   }
 
   // 路灯4
-  private _road4 = false;
+  private _road4 = true;
   public set road4(v: boolean) {
-    console.log('road4', v);
+    if (!this.roadController4) return;
     // 此处设置路灯4开关
     v ? this.roadController4.turnOn() : this.roadController4.turnOff();
-    this.lampState[3] = v;
     this._road4 = v;
   }
   public get road4() {
@@ -73,11 +68,6 @@ export class LampComponent implements OnInit, OnDestroy {
   constructor(private _diva: DivaService, private _data: DataService) {}
 
   async ngOnInit() {
-    this.lampState = this._data.lampState;
-    this.road1 = this.lampState[0];
-    this.road2 = this.lampState[1];
-    this.road3 = this.lampState[2];
-    this.road4 = this.lampState[3];
     this._diva.client.applyScene('灯光控制');
     this._data.changeCode(`client.applyScene('灯光控制')`);
     this.roadController1 = new DeviceController();
@@ -92,6 +82,10 @@ export class LampComponent implements OnInit, OnDestroy {
     this._data.changeCode(`client.getEntitiesByName<Light>('路灯03')`);
     this.road04 = (await this._diva.client.getEntitiesByName<Light>('路灯04'))[0];
     this._data.changeCode(`client.getEntitiesByName<Light>('路灯04')`);
+    this.road01.bind(this.roadController1.signal);
+    this.road02.bind(this.roadController2.signal);
+    this.road03.bind(this.roadController3.signal);
+    this.road04.bind(this.roadController4.signal);
     // this.road01.signal = this.roadController1.signal;
     // this.road02.signal = this.roadController2.signal;
     // this.road03.signal = this.roadController3.signal;
@@ -99,6 +93,5 @@ export class LampComponent implements OnInit, OnDestroy {
   }
 
   ngOnDestroy() {
-    this._data.lampState = this.lampState;
   }
 }
