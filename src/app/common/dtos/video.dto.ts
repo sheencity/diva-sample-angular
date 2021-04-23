af0c7673a5a4824d406c7aeabc9756a9d32fcd7c
郝强强 <haoqiangqiang@sheencity.com>
2021-04-23 10:17:02 +0800

    chore(diva-sdk): 通过索引值播放路径

--- src/app/common/dtos/video.dto.ts
+++ src/app/common/dtos/video.dto.ts
@@ -1,4 +1,5 @@
 import { Expose } from 'class-transformer';
 export class  VideoConfigDto {
     @Expose() public readonly title?: string;
+    @Expose() public readonly index?: number;
 }
--- src/app/pages/video/video.component.ts
+++ src/app/pages/video/video.component.ts
@@ -6,33 +6,43 @@ import { DivaService } from 'src/app/common/services/diva.service';
 const videos = [
   {
     title: '测试路径01',
+    index: 0,
   },
   {
     title: '测试路径02',
+    index: 1,
   },
   {
     title: '测试路径03',
+    index: 2,
   },
   {
     title: '测试路径04',
+    index: 3,
   },
   {
     title: '测试路径05',
+    index: 4,
   },
   {
     title: '测试路径06',
+    index: 5,
   },
   {
     title: '测试路径07',
+    index: 6,
   },
   {
     title: '测试路径08',
+    index: 7,
   },
   {
     title: '测试路径09',
+    index: 8,
   },
   {
     title: '测试路径10',
+    index: 9,
   },
 ];
 
@@ -53,8 +63,8 @@ export class VideoComponent implements OnInit, OnDestroy {
   async toggleVideo(video: VideoConfigDto) {
     this.selectedVideo = video.title;
     await this._diva.client.request('StopCameraTrack');
-    await this._diva.client.request('PlayCameraTrack', {
-      name: video.title,
+    await this._diva.client.request('PlayCameraTrackByIndex', {
+      index: video.index,
     });
     this._data.changeCode(`client.PlayCameraTrack(${video.title})`);
   }
