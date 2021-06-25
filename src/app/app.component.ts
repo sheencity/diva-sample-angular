f2545490ba4d5cd2217cc84746646f1360b06e24
何沁 <heqin@corp.sheencity.com>
2021-06-25 16:07:58 +0800

    feat: 增加自适应分辨率的功能

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,18 +1,47 @@
-import { Component, ElementRef, ViewChild } from '@angular/core';
+import {
+  AfterViewInit,
+  Component,
+  ElementRef,
+  OnDestroy,
+  ViewChild,
+} from '@angular/core';
+import { Subject, Subscription } from 'rxjs';
+import { debounceTime } from 'rxjs/operators';
 import { DivaService } from './common/services/diva.service';
 
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
-  styleUrls: ['./app.component.scss']
+  styleUrls: ['./app.component.scss'],
 })
-export class AppComponent {
+export class AppComponent implements AfterViewInit, OnDestroy {
+  @ViewChild('backendContainer', { read: ElementRef })
+  backendContainer?: ElementRef<HTMLElement>;
+  private _changeResolution$ = new Subject<boolean>();
+  private _subs: Subscription;
 
-  @ViewChild('backendContainer', { read: ElementRef }) backendContainer?: ElementRef;
   constructor(private _divaSer: DivaService) {}
-  public ngAfterViewInit() {
+
+  private _updateResolution = () => {
+    const height = this.backendContainer.nativeElement.clientHeight;
+    const width = this.backendContainer.nativeElement.clientWidth;
+    this._divaSer.client.setResolution({ width, height });
+  }
+
+  public async ngAfterViewInit() {
     if (this.backendContainer) {
-      this._divaSer.init(this.backendContainer.nativeElement)
+      await this._divaSer.init(this.backendContainer.nativeElement);
+      this._updateResolution();
+      window.onresize = () => {
+        this._changeResolution$.next(true);
+      };
     }
+    this._subs = this._changeResolution$
+      .pipe(debounceTime(200))
+      .subscribe(this._updateResolution);
+  }
+
+  ngOnDestroy() {
+    this._subs.unsubscribe();
   }
 }
