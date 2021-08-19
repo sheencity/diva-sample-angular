b91b1f9db10f317fe588bf51c9614ccb66b4838f
陈志强 <chenzhiqiang@corp.sheencity.com>
2021-08-19 13:39:02 +0800

    feat: 增加摇杆和全屏切换开关

--- src/app/common/components/header/header.component.ts
+++ src/app/common/components/header/header.component.ts
@@ -14,7 +14,49 @@ export class HeaderComponent implements OnInit {
   public get exampleCode() {
     return this._exampleCode;
   }
+
+  private _joyStick = false;
+  public set joyStick(v: boolean) {
+    this._joyStick = v;
+  }
+  public get joyStick() {
+    return this._joyStick;
+  }
+
+  private _fullScreen = false;
+  public set fullScreen(v: boolean) {
+    this._fullScreen = v;
+  }
+  public get fullScreen() {
+    return this._fullScreen;
+  }
+
   constructor() { }
+
+  public toggleJoystick(event: boolean): void {
+    this.joyStick = event;
+    const joystickEle = document.getElementById('joyStick');
+    if (joystickEle) {
+      this.joyStick ? joystickEle.style.display = 'block' : joystickEle.style.display = 'none';
+    }
+  }
+
+  public toggleFullScreen(event: boolean): void {
+    this.fullScreen = event;
+    this.fullScreen ? this.getFullScreen(document.documentElement) : this.exitFullScreen();
+  }
+
+  public getFullScreen(ele: Element): void {
+    if (ele.requestFullscreen) {
+      ele.requestFullscreen();
+    }
+  }
+
+  public exitFullScreen(): void {
+    if (document.exitFullscreen) {
+      document.exitFullscreen();
+    }
+  }
 
   ngOnInit(): void {
   }
