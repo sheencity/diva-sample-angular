d530538d94f728e8734acdc938a03e1254aa2b88
Wang Zishi <ynh.2@outlook.com>
2021-11-16 11:56:26 +0800

    feat: 自适应适配器

--- src/app/common/services/diva.service.ts
+++ src/app/common/services/diva.service.ts
@@ -1,34 +1,36 @@
 import { Injectable } from '@angular/core';
-import { Diva, DivaClient } from '@sheencity/diva-sdk';
+import type { DivaClient } from '@sheencity/diva-sdk';
+import { Diva } from '@sheencity/diva-sdk';
+import type { Adapter } from '@sheencity/diva-sdk-adapter';
 import { CefAdapter, WebRtcAdapter } from '@sheencity/diva-sdk-adapter';
 
 @Injectable({
   providedIn: 'root',
 })
 export class DivaService {
-  // divaClient
-  client?: DivaClient;
+  #client?: DivaClient;
 
-  constructor() {}
+  public get client() {
+    if (!this.#client) new Error('diva client is not initialized');
+    return this.#client;
+  }
 
   /**
    * 初始话 webRtc 链接
    * @param container (HTMLElement) 视频加载的 dom 元素
    */
-  async init(container: HTMLDivElement) {
+  public async init(container: HTMLDivElement) {
     console.log({ container });
     const apiKey = '<replace_your_api_key_here>';
-
-    // 使用云渲染模式
-    const uri = new URL('ws://127.0.0.1:3000');
-    const adapter = new WebRtcAdapter(container, uri);
-
-    // 使用内嵌模式
-    // const adapter = new CefAdapter(container);
+    const adapter: Adapter = /Mars/.test(globalThis.navigator.userAgent)
+      ? new CefAdapter(container) // 使用内嵌模式
+      : new WebRtcAdapter(container, new URL('ws://127.0.0.1:3000')); // 使用云渲染模式
 
     const diva = new Diva({ apiKey, adapter });
     console.log('diva is', diva);
-    this.client = await diva.init();
+
+    this.#client = await diva.init();
+
     console.log('client is', this.client);
   }
 }
